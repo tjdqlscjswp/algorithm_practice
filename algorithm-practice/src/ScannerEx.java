1cc91c2cbc6d180e459d05e1817e35a9a8c9c240
sungbeen <tjdqlscjswp@naver.com>
2023-07-02 15:01:47 +0900

    0702 algorithm commit
    testing commit of algorithm problems

--- algorithm-practice/src/ScannerEx.java
+++ algorithm-practice/src/ScannerEx.java
@@ -1,15 +1,14 @@
 import java.util.Scanner;
 
-//Main 클래스에서 Solution클래스 선언해주기
-public class Main {
-    public static void main(String[] ars) {
+public class ScannerEx {
+    public int addNumbers(){
         Scanner sc = new Scanner(System.in);
-
         int num1;
         int num2;
         num1 = sc.nextInt();
         num2 = sc.nextInt();
 
         System.out.println(num1 + num2);
-        }
+        return num1+num2;
     }
+}
