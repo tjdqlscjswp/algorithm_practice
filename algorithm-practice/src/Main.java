1370436353ee7d06c84e6b78d70f12ab8c0a43b8
sungbeen <tjdqlscjswp@naver.com>
2023-07-02 14:45:41 +0900

    Initial commit

---
+++ algorithm-practice/src/Main.java
@@ -0,0 +1,9 @@
+//Main 클래스에서 Solution클래스 선언해주기
+public class Main {
+    public static void main(String[] ars) {
+        Solution s = new Solution();
+        //입력요소를 선언해줘야 출력값이 나옴
+        int[] a = {5, 9, 7, 10};
+        s.solution(a, 6);
+    }
+}
---
+++ algorithm-practice/src/Solution.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+// 알고리즘 문제
+class Solution {
+    public int[] solution(int[] arr, int divisor) {
+
+
+        int[] answer;
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] % divisor == 0){
+                list.add(arr[i]);
+            }
+        }
+
+        if(list.isEmpty()){
+            list.add(-1);
+        }
+
+        answer = new int[list.size()];
+
+        for(int i=0; i<list.size(); i++){
+            answer[i] = list.get(i);
+        }
+
+        System.out.println(Arrays.toString(answer));
+        return answer;
+    }
+}
